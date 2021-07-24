a2e4d1c3e9197cf0393c0612bbb58b53e4426d7e
leeboa2005 <leeboa2005@naver.com>
2021-07-24 19:03:17 +0900

    0724

--- src/components/Ham.js
+++ src/components/Ham.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import $ from 'jquery';
 
-function Ham() {
-  return (
-    <>
-      <div class="hamburger-button">
+class App extends React.Component {
+  componentDidMount(){
+    $('.hamburger-button').on('click', function(event) {
+      event.preventDefault();
+      $(this).toggleClass('active');
+      $('.overlay').toggleClass('visible');
+  });
+  }
+  render(){
+    return (
+      <>
+      <Link to="#" class="hamburger-button">
         <span></span>
         <span></span>
         <span></span>
         <span></span>
-      </div>
+      </Link>
 
       <div class="overlay">
         <nav class="menu">
@@ -33,6 +42,46 @@ function Ham() {
         </nav>
       </div>
     </>
-  );
+    );
+  }
 }
-export default Ham;
+
+export default App;
+
+
+// function Ham() {
+//   return (
+//     <>
+//       <Link to="#"  class="hamburger-button">
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//         <span></span>
+//       </Link>
+
+//       <div class="overlay">
+//         <nav class="menu">
+//           <ul>
+//             <li>
+//               <Link to="/">ARMARIO</Link>
+//             </li>
+//             <li>
+//               <Link to="./review">REVIEW</Link>
+//             </li>
+//             <li>
+//               <Link to="./service">SERVICE</Link>
+//             </li>
+//             <li>
+//               <Link to="./event">EVENT</Link>
+//             </li>
+//             <li>
+//               <Link to="./store">STORE</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>
+//     </>
+//   );
+// }
+// export default Ham;
+
